5fc1d61024e7beec39abd36d2a03c7691d5e7bd3
Phan Đình Minh Hiếu <16520412@gm.uit.edu.vn>
2018-09-13 10:42:56 +0700

    update footer+ header + add bootstrap

---
+++ resources/views/header.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+        <title>Laravel</title>
+			
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+		
+		<!-- CSS -->
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+		<!-- JS -->
+		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+		 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    </head>
+    <body>
