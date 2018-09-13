b735697d3bf06a1a5f7b03713de2d8577db1fdef
Ha Kiet Hung <16520470@gm.uit.edu.vn>
2018-09-13 15:40:30 +0700

    task: tạo footer cho page theo sample qualuuniemh2\Document\footer_sample.png

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,27 @@
 	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>Maker</h3>
+				<li></li>
+			</div>
+			
+			<div class="col-sm-4">
+				<h3>MEET MAKER</h3>
+				<li>About Maker</li>
+				<li>Explore our Ecosystem</li>
+				<li>Careers</li>
+				<li>Community Meetups</li>
+				<li>Help Center</li>
+				<li>Contact Us</li>
+			</div>
+			
+			
+			<div class="col-sm-4">
+				<h3>Follow Us</h3>
+			</div>
+		</div>
+	</div>
 	</footer>
 	</body>
 </html>
