45a5d94d546da771a6b5f5f7bf545f8f8a83344d
Phan Đình Minh Hiếu <16520412@gm.uit.edu.vn>
2018-09-14 01:51:56 +0700

    update homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 @include('header')
+	<div id="hp">
+		<div class="hp_fullwidth_img">
+			<h3>H2 MANG CẢ THẾ GIỚI ĐẾN VỚI NHÀ CỦA BẠN</h3>
+		</div>
 
-	<div class="hp_fullwidth_img">
-	
-	</div>
 
-
-	<div class="container">
+		<div class="container">
 		
+		
+			
+		</div>
 	</div>
-	
 @include('footer')
--- storage/framework/views/49fd60bed1da289f274d655a55bb31a22acd3620.php
+++ storage/framework/views/49fd60bed1da289f274d655a55bb31a22acd3620.php
@@ -1,12 +1,14 @@
 <?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+	<div id="hp">
+		<div class="hp_fullwidth_img">
+			<h3>H2 MANG CẢ THẾ GIỚI ĐẾN VỚI NHÀ CỦA BẠN</h3>
+		</div>
 
-	<div class="hp_fullwidth_img">
-	
-	</div>
 
-
-	<div class="container">
+		<div class="container">
 		
+		
+			
+		</div>
 	</div>
-	
 <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
