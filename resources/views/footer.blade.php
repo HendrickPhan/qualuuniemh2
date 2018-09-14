b7b3f9d1d64695f3d476207b7c5c40b20eb97edc
Ha Kiet Hung <16520470@gm.uit.edu.vn>
2018-09-14 22:20:29 +0700

    Task ngày 14/7/2018

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,23 +2,23 @@
 		<div class="cover">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-4">
-						<h3>Maker</h3>
+					<div class="col-sm-3">
+						<h3><a>Maker</a></h3>
 						<li></li>
 					</div>
 					
-					<div class="col-sm-4">
-						<h3>MEET MAKER</h3>
-						<li>About Maker</li>
-						<li>Explore our Ecosystem</li>
-						<li>Careers</li>
-						<li>Community Meetups</li>
-						<li>Help Center</li>
-						<li>Contact Us</li>
+					<div class="col-sm-5">
+						<h3><a>MEET MAKER</a></h3>
+						<li><a href="">About Maker</a></li>
+						<li><a href="">Explore our Ecosystem</a></li>
+						<li><a href="">Careers</a></li>
+						<li><a href="">Community Meetups</a></li>
+						<li><a href="">Help Center</a></li>
+						<li><a href="">Contact Us</a></li>
 					</div>
 					
 					
-					<div class="col-sm-4">
+					<div class="col-sm-3">
 						<h3>Follow Us</h3>
 						<img src="upload/facebook.png"/>
 						<img src="upload/google.png"/>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -25,26 +25,28 @@
 		<div class="container">
 			<div class="row header-menu">
 				<div class="col-sm-3">
-					<img src="upload/logo/png"/>
+					<img src="upload/logo.png" width="50" height="50"/>
+					<li>International Company </li>
 				</div>
-				<div class="col-sm-5">
-					<input type="text" value="Search for Products" id="searchproduct"/>
-					<button class="btn" type="button">Search</button>			
+				<div class="col-sm-5" id="search-menu">
+					<input type="text" value="Search for Products" id="searchproduct"><button class="btn" type="button" id="searchbutton"><b>Search</b></button></input>	
+							
 				</div>
-				<div class="col-sm-4">
-					<li>800.860.6272</li>
-					<li>Sign in</li>
-					<li>Cart</li>
+				<div class="col-sm-4" id="info-menu">
+					<img src="upload/phone.png" width="25" height="25"/>
+					<li><a href="">800.860.6272</a></li>
+					<li><img src="upload/sign_in.png" width="25" height="25"/><a href="">Sign in</a></li>
+					<li><img src="upload/product.png" width="25" height="25"/><a href="">Cart</a></li>
 				</div>
 			</div>
 		</div>
 		<nav class="nav-menu">
-			<li>Dây chuyền</li>
-			<li>Nhẫn</li>
-			<li>Quần áo</li>
-			<li>Đồng hồ</li>
-			<li>Vòng tay</li>
-			<li>Gấu bông</li>
-			<li>Khăn choàng</li>
+			<li><a  href=""><b>Dây chuyền</b></a></li>
+			<li><a  href=""><b>Nhẫn</b></a></li>
+			<li><a  href=""><b>Quần áo</b></a></li>
+			<li><a  href=""><b>Đồng hồ</b></a></li>
+			<li><a  href=""><b>Vòng tay</b></a></li>
+			<li><a  href=""><b>Gấu bông</b></a></li>
+			<li><a  href=""><b>Khăn choàng</b></a></li>
 		</nav>
 	</header>
